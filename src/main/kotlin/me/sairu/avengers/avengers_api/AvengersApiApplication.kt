2db3911f98193b852c4b3fa187d21eb1a6ea96fd
[Saiiru] <[sairufsa@gmail.com]>
2024-09-16 12:24:37 -0300

    initial commit

---
+++ src/main/kotlin/me/sairu/avengers/avengers_api/AvengersApiApplication.kt
@@ -0,0 +1,11 @@
+package me.sairu.avengers.avengers_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvengersApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AvengersApiApplication>(*args)
+}
